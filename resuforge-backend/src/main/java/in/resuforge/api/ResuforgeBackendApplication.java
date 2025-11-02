63165eebffe692e64664109da77e225c8d037bba
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-11-02 18:25:28 +0530

    Project Created and Actuator Dependency and Mongodb Configurations Added

---
+++ resuforge-backend/src/main/java/in/resuforge/api/ResuforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package in.resuforge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResuforgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResuforgeBackendApplication.class, args);
+	}
+
+}
