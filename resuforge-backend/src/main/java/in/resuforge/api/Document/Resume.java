a3cc6194aef9d074be3278fc295f1cdbd9f5f843
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2026-02-03 20:53:30 +0530

    Resume document

--- resuforge-backend/src/main/java/in/resuforge/api/Document/Resume.java
+++ resuforge-backend/src/main/java/in/resuforge/api/Document/Resume.java
@@ -13,7 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Document(collation = "resumes")
+@Document(collection = "resumes")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
