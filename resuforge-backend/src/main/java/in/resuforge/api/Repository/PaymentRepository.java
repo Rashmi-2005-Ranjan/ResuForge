7f49fb277b9433dda430a018b6c5ff8d4fb207bf
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2026-02-08 23:41:00 +0530

    Created Payment Document and Payment Repository

---
+++ resuforge-backend/src/main/java/in/resuforge/api/Repository/PaymentRepository.java
@@ -0,0 +1,20 @@
+package in.resuforge.api.Repository;
+
+import in.resuforge.api.Document.Payment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends MongoRepository<Payment,String> {
+
+    Optional<Payment> findByRazorpayOrderId(String razorpayOrderId);
+
+    Optional<Payment> findByRazorpayPaymentId(String razorpayPaymentId);
+
+    List<Payment> findByUserIdOrderByCreatedAtDesc(String userId);
+
+    List<Payment> findByStatus(String status);
+}
