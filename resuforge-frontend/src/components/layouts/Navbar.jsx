464ab1d4cce6c62e366e5a80ebaae35e7cbfa76a
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2026-02-12 22:21:10 +0530

    Navbar Component

--- resuforge-frontend/src/components/layouts/Navbar.jsx
+++ resuforge-frontend/src/components/layouts/Navbar.jsx
@@ -4,18 +4,24 @@ import { Link } from "react-router-dom";
 import { FileText } from "lucide-react";
 
 const Navbar = () => {
-  return <div className="h-16 bg-white border boredr-b border-gray-200/50 backdrop-blur-[2px] py-2.5 px-4 md:px-0 sticky top-0 z-30">
-      <div className="container mx-auto flex items-center justify-between gap-5">
-        <Link to='/dashboard'>
-          <h2 className="text-lg md:text-xl font-medium text-black leading-5 flex items-center gap-2">
-            <FileText className="w-5 h-5 text-blue-600" />
-            Resume Builder
-          </h2>
-        </Link>
+  return (
+      <div className="h-16 backdrop-blur-xl bg-slate-950/80 border-b border-slate-800/50 py-2.5 px-4 md:px-0 sticky top-0 z-30">
+        <div className="container mx-auto flex items-center justify-between gap-5">
+          <Link to='/dashboard'>
+            <h2 className="text-lg md:text-xl font-semibold leading-5 flex items-center gap-2 group">
+              <div className="p-2 bg-gradient-to-br from-blue-500 to-purple-600 rounded-lg group-hover:scale-110 transition-transform duration-300">
+                <FileText className="w-4 h-4 text-white" />
+              </div>
+              <span className="bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
+              Resume Builder
+            </span>
+            </h2>
+          </Link>
 
-        <ProfileInfoCard />
+          <ProfileInfoCard />
+        </div>
       </div>
-    </div>
+  );
 };
 
 export default Navbar;
