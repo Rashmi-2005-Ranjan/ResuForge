3b90b11e3738620a89ff1d957ae6db952737f01b
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2026-02-23 23:07:53 +0530

    Updated apiPaths.js

--- resuforge-frontend/src/utils/apiPaths.js
+++ resuforge-frontend/src/utils/apiPaths.js
@@ -1,29 +1,27 @@
-export const BASE_URL = "http://localhost:8080";
-
-// utils/apiPaths.js
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register",
     LOGIN: "/api/auth/login",
     PROFILE: "/api/auth/profile",
     UPLOAD_IMAGE: "/api/auth/upload-image",
-    TEMPLATES: "/api/templates",
+    TEMPLATES: "/api/templates/get-templates",
     RESEND_VERIFICATION: "/api/auth/resend-verification",
     VERIFY_EMAIL: "/api/auth/verify-email",
   },
   RESUME: {
-    CREATE: "/api/resumes",
-    GET_ALL: "/api/resumes",
+    CREATE: "/api/resumes/create",
+    GET_ALL: "/api/resumes/get-user-resumes",
     GET_BY_ID: (id) => `/api/resumes/${id}`,
-    UPDATE: (id) => `/api/resumes/${id}`,
-    DELETE: (id) => `/api/resumes/${id}`,
-    UPLOAD_IMAGES: (id) => `/api/resumes/${id}/upload-images`,
+    UPDATE: (id) => `/api/resumes/update-resume/${id}`,
+    DELETE: (id) => `/api/resumes/delete-resume/${id}`,
+    UPLOAD_IMAGES: (resumeId) => `/api/resumes/upload-resume-images/${resumeId}`,
   },
   PAYMENT: {
-    CREATE_ORDER: "/api/payment/create-order",
-    VERIFY: "/api/payment/verify",
-    HISTORY: "/api/payment/history",
-    ORDER_DETAILS: (orderId) => `/api/payment/order/${orderId}`,
+    CREATE_ORDER: "/api/payments/create-order",
+    VERIFY: "/api/payments/verify",
+    HISTORY: "/api/payments/history",
+    ORDER_DETAILS: (orderId) => `/api/payments/order/${orderId}`,
   },
   EMAIL: {
     SEND_RESUME: "/api/email/send-resume",
