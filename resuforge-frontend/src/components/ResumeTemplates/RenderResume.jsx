ae26a3398ad9de8930ab10ae81f74d9a8262e40b
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2026-02-24 18:38:55 +0530

    Render Resume Component and Resume Summary Card

--- resuforge-frontend/src/components/ResumeTemplates/RenderResume.jsx
+++ resuforge-frontend/src/components/ResumeTemplates/RenderResume.jsx
@@ -4,45 +4,45 @@ import TemplateTwo from "./TemplateTwo";
 import TemplateThree from "./TemplateThree";
 
 const RenderResume = ({
-  templateId,
-  resumeData,
-  colorPalette,
-  containerWidth,
-}) => {
-  switch (templateId) {
-    case "01":
-      return (
-        <TemplateOne
-          resumeData={resumeData}
-          colorPalette={colorPalette}
-          containerWidth={containerWidth}
-        />
-      );
-    case "02":
-      return (
-        <TemplateTwo
-          resumeData={resumeData}
-          colorPalette={colorPalette}
-          containerWidth={containerWidth}
-        />
-      );
-    case "03":
-      return (
-        <TemplateThree
-          resumeData={resumeData}
-          colorPalette={colorPalette}
-          containerWidth={containerWidth}
-        />
-      );
-    default:
-      return (
-        <TemplateOne
-          resumeData={resumeData}
-          colorPalette={colorPalette}
-          containerWidth={containerWidth}
-        />
-      );
-  }
+                          templateId,
+                          resumeData,
+                          colorPalette,
+                          containerWidth,
+                      }) => {
+    switch (templateId) {
+        case "01":
+            return (
+                <TemplateOne
+                    resumeData={resumeData}
+                    colorPalette={colorPalette}
+                    containerWidth={containerWidth}
+                />
+            );
+        case "02":
+            return (
+                <TemplateTwo
+                    resumeData={resumeData}
+                    colorPalette={colorPalette}
+                    containerWidth={containerWidth}
+                />
+            );
+        case "03":
+            return (
+                <TemplateThree
+                    resumeData={resumeData}
+                    colorPalette={colorPalette}
+                    containerWidth={containerWidth}
+                />
+            );
+        default:
+            return (
+                <TemplateOne
+                    resumeData={resumeData}
+                    colorPalette={colorPalette}
+                    containerWidth={containerWidth}
+                />
+            );
+    }
 };
 
 export default RenderResume;
